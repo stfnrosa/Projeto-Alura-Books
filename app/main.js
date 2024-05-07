dfd7b2a0087d332712ed3adf4c417aa0a67d77fa
stfnrosa <rosaa.estefanii@gmail.com>
2024-05-07 18:05:48 -0300

    Aplicando métodos de array

--- app/main.js
+++ app/main.js
@@ -2,10 +2,11 @@ let livros = [];
 const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosDaApi();
 
+
 async function getBuscarLivrosDaApi(){
     const resposta = await fetch(endpointDaApi);
     livros = await resposta.json();
-    console.table(livros);
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto);
 }
 
-//Imagine que você tenha uma grande quantidade de livros. Sem o await, a função getBuscarLivrosDaAPI() retornaria a Promise imediatamente, e outras partes do seu código poderiam tentar acessar a variável livros antes dela estar devidamente preenchida, causando problemas de sincronização.
